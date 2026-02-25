89e3707a7e0deaaf032574d777256bc38b90dfdf
Rattasat Phabsing <jkung1998@gmail.com>
2026-02-25 18:01:09 +0700

    fff

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -4,10 +4,10 @@ if (!admin.apps.length) {
     try {
         admin.initializeApp({
             credential: admin.credential.cert({
-                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '',
                 // Handle newlines in the private key
-                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
             }),
         });
     } catch (error) {
